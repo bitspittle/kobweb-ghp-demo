21b2d64d208594aabc805f3078af68612f98cabc
David Herman <bitspittle@gmail.com>
2022-02-10 18:06:41 -0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kobweb.application)
+    alias(libs.plugins.kobwebx.markdown)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+    maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+}
+
+group = "org.example.site"
+version = "1.0-SNAPSHOT"
+
+kotlin {
+    jvm {
+        tasks.withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = "11"
+        }
+
+        tasks.named("jvmJar", Jar::class.java).configure {
+            archiveFileName.set("kobwebghpdemo.jar")
+        }
+    }
+    js(IR) {
+        moduleName = "kobwebghpdemo"
+        browser {
+            commonWebpackConfig {
+                outputFileName = "kobwebghpdemo.js"
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(libs.kobweb.core)
+                implementation(libs.kobweb.silk.core)
+                implementation(libs.kobweb.silk.icons.fa)
+                implementation(libs.kobwebx.markdown)
+             }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(libs.kobweb.api)
+             }
+        }
+    }
+}
---
+++ src/jsMain/kotlin/org/example/site/MyApp.kt
@@ -0,0 +1,44 @@
+package org.example.site
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.core.App
+import com.varabyte.kobweb.silk.InitSilk
+import com.varabyte.kobweb.silk.InitSilkContext
+import com.varabyte.kobweb.silk.SilkApp
+import com.varabyte.kobweb.silk.components.layout.Surface
+import com.varabyte.kobweb.silk.theme.colors.ColorMode
+import com.varabyte.kobweb.silk.theme.colors.getColorMode
+import com.varabyte.kobweb.silk.theme.registerBaseStyle
+import kotlinx.browser.localStorage
+import org.jetbrains.compose.web.css.*
+
+private const val COLOR_MODE_KEY = "kobwebghpdemo:colorMode"
+
+@InitSilk
+fun updateTheme(ctx: InitSilkContext) {
+    ctx.config.initialColorMode = localStorage.getItem(COLOR_MODE_KEY)?.let { ColorMode.valueOf(it) } ?: ColorMode.LIGHT
+
+    ctx.config.registerBaseStyle("body") {
+        Modifier.fontFamily(
+	    "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
+            "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif"
+        )
+    }
+}
+
+@App
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    SilkApp {
+        val colorMode = getColorMode()
+        LaunchedEffect(colorMode) {
+            localStorage.setItem(COLOR_MODE_KEY, colorMode.name)
+        }
+
+        Surface(Modifier.minHeight(100.vh)) {
+            content()
+        }
+    }
+}
